9b22bb713faf9b6861848c6366c2cfe06f85fbf2
blaws006 <blaws006>
2018-01-07 19:54:04 -0500

    Can connect to the SQL database from here.

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
+
+
+var env = require('dotenv').load();
+
+//Expressjs
 var express = require('express');
-var passport = require('passport');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-
-
-
 var app = express();
 
 //middleware
 
 //Body Parser
+var bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //For Passport
+var passport = require('passport');
+var session = require('express-session');
 
 app.use(session({
     secret: 'b-law',
@@ -31,6 +34,15 @@ app.get('/', function (req, res) {
 
 });
 
+//Synch Database
+var models = require("./app/models");
+
+models.sequelize.sync().then(function() {
+  console.log("Nice! Database looks fine!")  
+})
+.catch(function(err){
+    console.log(err, "Something went wrong with the database Update!")
+});
 
 app.listen(5000, function (err) {
 
